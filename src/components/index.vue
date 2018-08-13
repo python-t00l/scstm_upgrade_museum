2a356653f76ad5f2751c44614ccf284ac6961311
sunshine824 <790168710@qq.com>
2018-08-13 17:54:29 +0800

    首页——四川科博场馆

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,6 +1,273 @@
 <template>
-  <div>
+  <div class="con">
+    <!--首页—顶部轮播、通知公告-->
+    <div class="floor-one clearfix">
+      <div class="swiper"></div>
+      <div class="notice-box">
+        <div class="title clearfix">
+          <p>
+            <span class="ch">通知公告</span>
+            <!--<span class="en">NOTICE ANNOUNCEMENT</span>-->
+          </p>
+          <router-link to=""> 更多></router-link>
+        </div>
+        <div class="notice-list">
+          <div class="thumbnail">
+            <router-link to="">
+              <img src="../assets/test/test1.png"/>
+              <p class="mask">开展音信十三人起步走活动</p>
+            </router-link>
+          </div>
+          <ul class="list">
+            <li>
+              <span class="right-blue-triangle"></span>
+              <router-link to="">关于科博场馆建设与发展的几点建议</router-link>
+            </li>
+            <li>
+              <span class="right-blue-triangle"></span>
+              <router-link to="">关于科博场馆建设与发展的几点建议</router-link>
+            </li>
+            <li>
+              <span class="right-blue-triangle"></span>
+              <router-link to="">关于科博场馆建设与发展的几点建议</router-link>
+            </li>
+            <li>
+              <span class="right-blue-triangle"></span>
+              <router-link to="">关于科博场馆建设与发展的几点建议</router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <!--首页—协会动态、协会党建-->
+    <div class="floor-two clearfix">
+      <div class="dynamics">
+        <div class="title clearfix">
+          <p>
+            <span class="ch">协会动态</span>
+            <span class="en">ASSOCIATION DYNAMICS</span>
+          </p>
+          <router-link to=""> 更多></router-link>
+        </div>
+        <div class="lists">
+          <div class="thumbnail clearfix">
+            <router-link to="">
+              <div class="img">
+                <img src="../assets/test/test1.png"/>
+              </div>
+              <div class="intr">
+                <p class="tit">畅游科技海洋，播撒科学的种子</p>
+                <p class="intr-txt">
+                  园——航模表演现场会在什邡市马井学校举行。
+                  德阳市科技馆馆长卢春涛，什邡市科协主席朱晓峰、
+                  副主席易佑全，什邡市教育局信息中心主任尹新明等领导莅临指导，
+                  来自全什邡市各兄弟学校的近40名科技。
+                </p>
+              </div>
+            </router-link>
+          </div>
+          <ul class="list clearfix">
+            <li>
+              <p class="txt">
+                <span class="right-blue-triangle"></span>
+                <router-link to="">关于科博场馆建设与发展的几点建议</router-link>
+              </p>
+              <p class="date">2018-07-14</p>
+            </li>
+            <li>
+              <p class="txt">
+                <span class="right-blue-triangle"></span>
+                <router-link to="">关于科博场馆建设与发展的几点建议</router-link>
+              </p>
+              <p class="date">2018-07-14</p>
+            </li>
+            <li>
+              <p class="txt">
+                <span class="right-blue-triangle"></span>
+                <router-link to="">关于科博场馆建设与发展的几点建议</router-link>
+              </p>
+              <p class="date">2018-07-14</p>
+            </li>
+          </ul>
+        </div>
+      </div>
 
+      <div class="party">
+        <div class="title clearfix">
+          <p>
+            <img class="ico" src="../assets/party.png"/>
+            <span class="ch">协会动态</span>
+            <span class="en">ASSOCIATION DYNAMICS</span>
+          </p>
+          <router-link to=""> 更多></router-link>
+        </div>
+        <div class="lists">
+          <div class="thumbnail clearfix">
+            <router-link to="">
+              <div class="img">
+                <img src="../assets/test/test1.png"/>
+              </div>
+              <div class="intr">
+                <p class="tit">畅游科技海洋，播撒科学的种子</p>
+                <p class="intr-txt">
+                  园——航模表演现场会在什邡市马井学校举行。
+                  德阳市科技馆馆长卢春涛，什邡市科协主席朱晓峰、
+                  副主席易佑全，什邡市教育局信息中心主任尹新明等领导莅临指导，
+                  来自全什邡市各兄弟学校的近40名科技。
+                </p>
+              </div>
+            </router-link>
+          </div>
+          <ul class="list clearfix">
+            <li>
+              <p class="txt">
+                <span class="right-blue-triangle"></span>
+                <router-link to="">关于科博场馆建设与发展的几点建议</router-link>
+              </p>
+              <p class="date">2018-07-14</p>
+            </li>
+            <li>
+              <p class="txt">
+                <span class="right-blue-triangle"></span>
+                <router-link to="">关于科博场馆建设与发展的几点建议</router-link>
+              </p>
+              <p class="date">2018-07-14</p>
+            </li>
+            <li>
+              <p class="txt">
+                <span class="right-blue-triangle"></span>
+                <router-link to="">关于科博场馆建设与发展的几点建议</router-link>
+              </p>
+              <p class="date">2018-07-14</p>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <!--增进馆际交流合作提高为社会服务能力-->
+    <div class="floor-three clearfix">
+      <div class="banner">
+        <img src="../assets/banner1.jpg"/>
+      </div>
+      <div class="member">
+        <div class="title clearfix">
+          <p>
+            <span class="ch">会员天地</span>
+            <span class="en">MEMBER CLUB</span>
+          </p>
+          <router-link to=""> 更多></router-link>
+        </div>
+        <ul class="list clearfix">
+          <li>
+            <div class="txt">
+              <span class="grad grad-green">会员咨询</span>
+              <router-link to="">
+                四川省科博协参加地方自然科学博物馆协会2016年工作会议
+              </router-link>
+            </div>
+            <p class="date">2018-07-14</p>
+          </li>
+          <li>
+            <div class="txt">
+              <span class="grad grad-blue">专项活动</span>
+              <router-link to="">
+                四川省科博协参加地方自然科学博物馆协会2016年工作会议
+              </router-link>
+            </div>
+            <p class="date">2018-07-14</p>
+          </li>
+          <li>
+            <div class="txt">
+              <span class="grad grad-origin">我要入会</span>
+              <router-link to="">
+                四川省科博协参加地方自然科学博物馆协会2016年工作会议
+              </router-link>
+            </div>
+            <p class="date">2018-07-14</p>
+          </li>
+          <li>
+            <div class="txt">
+              <span class="grad grad-originred">继续教育</span>
+              <router-link to="">
+                四川省科博协参加地方自然科学博物馆协会2016年工作会议
+              </router-link>
+            </div>
+            <p class="date">2018-07-14</p>
+          </li>
+          <li>
+            <div class="txt">
+              <span class="grad grad-red">赛事纵览</span>
+              <router-link to="">
+                四川省科博协参加地方自然科学博物馆协会2016年工作会议
+              </router-link>
+            </div>
+            <p class="date">2018-07-14</p>
+          </li>
+        </ul>
+      </div>
+      <div class="system">
+        <div class="title clearfix">
+          <p>
+            <span class="ch">现代科技馆体系</span>
+            <span class="en">TECHNOLOGY MUSEUM SYSTEM</span>
+          </p>
+          <router-link to=""> 更多></router-link>
+        </div>
+        <ul class="list clearfix">
+          <li>
+            <div class="txt">
+              <span class="grad grad-green">会员咨询</span>
+              <router-link to="">
+                四川省科博协参加地方自然科学博物馆协会2016年工作会议
+              </router-link>
+            </div>
+            <p class="date">2018-07-14</p>
+          </li>
+          <li>
+            <div class="txt">
+              <span class="grad grad-blue">专项活动</span>
+              <router-link to="">
+                四川省科博协参加地方自然科学博物馆协会2016年工作会议
+              </router-link>
+            </div>
+            <p class="date">2018-07-14</p>
+          </li>
+          <li>
+            <div class="txt">
+              <span class="grad grad-origin">我要入会</span>
+              <router-link to="">
+                四川省科博协参加地方自然科学博物馆协会2016年工作会议
+              </router-link>
+            </div>
+            <p class="date">2018-07-14</p>
+          </li>
+          <li>
+            <div class="txt">
+              <span class="grad grad-originred">继续教育</span>
+              <router-link to="">
+                四川省科博协参加地方自然科学博物馆协会2016年工作会议
+              </router-link>
+            </div>
+            <p class="date">2018-07-14</p>
+          </li>
+          <li>
+            <div class="txt">
+              <span class="grad grad-red">赛事纵览</span>
+              <router-link to="">
+                四川省科博协参加地方自然科学博物馆协会2016年工作会议
+              </router-link>
+            </div>
+            <p class="date">2018-07-14</p>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <!--四川科博场馆-->
+    <div class="museum">
+      <div class="banner">
+        <img src="../assets/banner2.jpg"/>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -14,5 +281,255 @@
 </script>
 
 <style scoped lang="less">
-
+  .con {
+    width: 1200px;
+    margin: 20px auto;
+    box-sizing: border-box;
+    padding: 20px;
+    background: #fff;
+    .title {
+      padding-bottom: 14px;
+      border-bottom: 1px solid #eee;
+      p {
+        float: left;
+        .ico {
+          width: 22px;
+          display: inline-block;
+          vertical-align: middle;
+          margin-right: 5px;
+        }
+        .ch {
+          font-size: 18px;
+          color: #333;
+          padding-right: 5px;
+          display: inline-block;
+          vertical-align: middle;
+          font-weight: bold;
+        }
+        .en {
+          font-size: 15px;
+          color: #989797;
+          display: inline-block;
+          vertical-align: middle;
+        }
+      }
+      a {
+        float: right;
+        font-size: 14px;
+        color: #959595;
+        margin-top: 3px;
+      }
+    }
+    .floor-one {
+      .swiper {
+        width: 820px;
+        height: 424px;
+        background: #333;
+        float: left;
+      }
+      .notice-box {
+        float: right;
+        width: 320px;
+        .notice-list {
+          margin-top: 20px;
+          .thumbnail {
+            width: 100%;
+            max-height: 214px;
+            overflow: hidden;
+            a {
+              position: relative;
+              display: block;
+              img {
+                width: 100%;
+              }
+              .mask {
+                width: 100%;
+                height: 40px;
+                background: rgba(0, 0, 0, .7);
+                position: absolute;
+                bottom: 0;
+                text-align: center;
+                line-height: 40px;
+                color: #fff;
+              }
+            }
+          }
+          .list {
+            margin-top: 15px;
+            li {
+              overflow: hidden;
+              text-overflow: ellipsis;
+              white-space: nowrap;
+            }
+            .right-blue-triangle {
+              width: 0;
+              height: 0;
+              border-width: 5px 0 5px 5px;
+              border-style: solid;
+              border-color: transparent transparent transparent #20A6FC; /*透明 透明 透明 灰*/
+              position: relative;
+              display: inline-block;
+              vertical-align: middle;
+              margin-right: 5px;
+            }
+            a {
+              font-size: 16px;
+              color: #333;
+              line-height: 34px;
+              display: inline-block;
+              vertical-align: middle;
+              overflow: hidden;
+              text-overflow: ellipsis;
+              white-space: nowrap;
+            }
+          }
+        }
+      }
+    }
+    .floor-two {
+      margin-top: 20px;
+      .lists{
+        margin-top: 20px;
+        .thumbnail {
+          .img {
+            width: 210px;
+            float: left;
+            overflow: hidden;
+            img {
+              width: 100%;
+            }
+          }
+          .intr {
+            float: right;
+            width: 344px;
+            .tit {
+              font-size: 18px;
+              color: #333;
+              overflow: hidden;
+              text-overflow: ellipsis;
+              white-space: nowrap;
+              margin-bottom: 5px;
+            }
+            .intr-txt{
+              font-size: 14px;
+              color: #d1d1d1;
+              line-height: 26px;
+              text-align: justify;
+            }
+          }
+        }
+        .list{
+          margin-top: 18px;
+          li{
+            overflow: hidden;
+            text-overflow: ellipsis;
+            white-space: nowrap;
+            margin-bottom: 10px;
+            .txt{
+              float: left;
+              .right-blue-triangle {
+                width: 0;
+                height: 0;
+                border-width: 5px 0 5px 5px;
+                border-style: solid;
+                border-color: transparent transparent transparent #20A6FC; /*透明 透明 透明 灰*/
+                position: relative;
+                display: inline-block;
+                vertical-align: middle;
+                margin-right: 5px;
+              }
+              a {
+                font-size: 16px;
+                color: #333;
+                line-height: 34px;
+                display: inline-block;
+                vertical-align: middle;
+                overflow: hidden;
+                text-overflow: ellipsis;
+                white-space: nowrap;
+              }
+            }
+            .date{
+              font-size: 14px;
+              color: #999;
+              float: right;
+              line-height: 34px;
+            }
+          }
+        }
+      }
+      .dynamics {
+        float: left;
+        width: 570px;
+      }
+      .party {
+        float: right;
+        width: 570px;
+      }
+    }
+    .banner{
+      width: 1200px;
+      margin: 40px 0 20px -20px;
+      img{
+        width: 100%;
+      }
+    }
+    .floor-three{
+      .member,.system{
+        width: 570px;
+        float: left;
+        .list{
+          margin-top: 15px;
+          li{
+            overflow: hidden;
+            margin-bottom: 10px;
+            .txt{
+              float: left;
+              .grad{
+                width: 68px;
+                height: 24px;
+                font-size: 10px;
+                color: #fff;
+                border-radius: 2px;
+                text-align: center;
+                line-height: 24px;
+                display: inline-block;
+                vertical-align: middle;
+                margin-right: 5px;
+              }
+              .grad-green{
+                background-image: linear-gradient(-225deg, #00D0FB 0%, #00ACF6 100%);
+              }
+              .grad-blue{
+                background-image: linear-gradient(-225deg, #00B09B 0%, #96C93D 100%);
+              }
+              .grad-origin{
+                background-image: linear-gradient(-235deg, #FF8960 16%, #FF62A5 100%);
+              }
+              .grad-originred{
+                background-image: linear-gradient(-235deg, #FF8960 16%, #FF62A5 100%);
+              }
+              .grad-red{
+                background-image: linear-gradient(-225deg, #F657B7 0%, #E92C81 100%);
+              }
+              a{
+                font-size: 14px;
+                color: #333;
+                line-height: 34px;
+                display: inline-block;
+                vertical-align: middle;
+              }
+            }
+            .date{
+              float: right;
+              line-height: 34px;
+            }
+          }
+        }
+      }
+      .system{
+        float: right;
+      }
+    }
+  }
 </style>
